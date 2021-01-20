becf37eb45bf792228a2ba3896acdb55156bcb03
amangupta37 <amangupta6121998@gmail.com>
2021-01-20 23:23:19 +0530

    added a js file

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const data = document.getElementById('input-data');
+const btn = document.getElementById('btn-data');
+
+btn.addEventListener("click", function(){
+
+const input = data.value;
+
+
+if(input === "")
+{
+    alert("Please Enter Some Input");
+}
+
+
+})
